316479c349e3abaf9aa55bf070aa6170bc0fd6b5
Mehrdad <mehrdadft1994@gmail.com>
2026-08-24 14:13:21 +0200

    fix: remove commented-out SSH security rule from network security group

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -40,19 +40,6 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = local.tags
-
-  # security_rule {
-  #   name                       = "AllowSSH"
-  #   priority                   = 1001
-  #   direction                  = "Inbound"
-  #   access                     = "Allow"
-  #   protocol                   = "Tcp"
-  #   source_port_range          = "*"
-  #   destination_port_range     = "22"
-  #   source_address_prefixes    = var.allowed_ips
-  #   destination_address_prefix = "*"
-  # }
-
 }
 
 resource "azurerm_public_ip" "pip" {
@@ -83,7 +70,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    # public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
