2bb628d1a59ba795338d50c36b89e0db0ca3a33d
Mehrdad <mehrdadft1994@gmail.com>
2026-08-24 10:32:02 +0200

    create a gcp directory template

---
+++ terraform/gcp/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# --- Network Infrastructure ---
+
+resource "google_compute_network" "vpc" {
+  name                    = var.vnet_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-main"
+  ip_cidr_range = var.subnet_prefix[0] # e.g. "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+# --- Firewall Rule (NSG Equivalent) ---
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = var.allowed_ips
+  target_tags   = ["ssh-allowed"]
+}
+
+# --- Compute Engine (VM Instance) ---
+
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.vm_size # e.g. "e2-small" or "e2-medium"
+  zone         = var.zone
+
+  tags   = ["ssh-allowed"]
+  labels = local.tags
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 30
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.id
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    # An empty access_config allocates an ephemeral public IP
+    access_config {}
+  }
+
+  # Injects SSH public key directly into instance metadata
+  metadata = {
+    ssh-keys               = "${var.admin_username}:${trimspace(file(pathexpand(var.pubkey)))}"
+    block-project-ssh-keys = "true"
+  }
+}
---
+++ terraform/gcp/variables.tf
@@ -0,0 +1,71 @@
+variable "region" {
+  description = "GCP region for the resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "zone" {
+  description = "GCP ZONE"
+  type = string
+  default = "eastus"
+}
+
+variable "gcp_project_id" {
+  description = "project id on gcp"
+  type = string
+  default = "project-demo-terraform"  
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+  default     = "vnet-demo"
+}
+
+variable "address_space" {
+  description = "CIDR block for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  description = "CIDR block for the subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "vm_name" {
+  description = "Name of the Linux VM"
+  type        = string
+  default     = "vm-demo"
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_D2als_v7"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureadmin"
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "allowed_ips" {
+  description = "List of allowed IP addresses for SSH access"
+  type        = list(string)
+  default     = []
+}
+
+variable "pubkey" {
+  description = "path to the ssh public key to be used"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
