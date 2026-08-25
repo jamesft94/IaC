ba02da8215f766ee99514e9a3a71493515bc1747
Mehrdad <mehrdadft1994@gmail.com>
2026-08-25 10:53:13 +0200

    fix: correct description formatting in security group for Tailscale connectivity

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -132,7 +132,7 @@ resource "aws_security_group" "sg" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    description = "allow outband traffic to connect to tailscale"
+    description      = "allow outband traffic to connect to tailscale"
   }
 
   tags = merge(local.tags, {
