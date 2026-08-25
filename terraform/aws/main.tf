1af50ee132c134c306a558be8c6e37a1f9063234
Mehrdad <mehrdadft1994@gmail.com>
2026-08-25 14:35:24 +0200

    fix: add VPC flow log resource for enhanced network monitoring

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -224,3 +224,10 @@ resource "aws_instance" "vm" {
     Name = var.vm_name
   })
 }
+
+resource "aws_flow_log" "vpc_flow_log" {
+  iam_role_arn    = aws_iam_role.flow_log_role.arn
+  log_destination = aws_cloudwatch_log_group.flow_log_group.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
