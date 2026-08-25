220ae81ffaf290c75ae14475e29ddea277de24ce
Mehrdad <mehrdadft1994@gmail.com>
2026-08-25 14:26:18 +0200

    fix: enable detailed monitoring for EC2 instance

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -196,6 +196,7 @@ resource "aws_instance" "vm" {
   subnet_id     = aws_subnet.subnet.id
   key_name      = aws_key_pair.key.key_name
   ebs_optimized = true
+  monitoring    = true
 
   vpc_security_group_ids = [
     aws_security_group.sg.id
