ce540b627baa6860ca1437812af5ed62d27a450a
Mehrdad <mehrdadft1994@gmail.com>
2026-08-25 15:30:54 +0200

    fix undeclared variables and logging.

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -26,6 +26,49 @@ resource "aws_vpc" "vpc" {
   })
 }
 
+resource "aws_cloudwatch_log_group" "flow_log_group" {
+  name              = "/aws/vpc/flow-logs/${var.vm_name}"
+  retention_in_days = 7
+
+  tags = local.tags
+}
+
+resource "aws_iam_role" "flow_log_role" {
+  name = "${var.vm_name}-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy" "flow_log_policy" {
+  name = "${var.vm_name}-flow-log-policy"
+  role = aws_iam_role.flow_log_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents"
+      ]
+      Resource = "${aws_cloudwatch_log_group.flow_log_group.arn}:*"
+    }]
+  })
+}
+
 # Public subnet required to host the NAT Gateway
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.vpc.id
@@ -229,5 +272,5 @@ resource "aws_flow_log" "vpc_flow_log" {
   iam_role_arn    = aws_iam_role.flow_log_role.arn
   log_destination = aws_cloudwatch_log_group.flow_log_group.arn
   traffic_type    = "ALL"
-  vpc_id          = aws_vpc.main.id
+  vpc_id          = aws_vpc.vpc.id
 }
