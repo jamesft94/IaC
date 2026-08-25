1447ba13219d5acb401f42836ab990d68f4124c2
Mehrdad <mehrdadft1994@gmail.com>
2026-08-25 14:28:44 +0200

    fix: add logging configuration to subnet resource

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -28,6 +28,12 @@ resource "google_compute_subnetwork" "subnet" {
   region                   = var.region
   network                  = google_compute_network.vpc.id
   private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 resource "google_compute_router" "router" {
